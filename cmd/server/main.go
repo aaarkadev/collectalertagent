3e139cf7201d0d9a5419c69da469c018a87be16f
Andrey <arkadev.a@akusherstvo.ru>
2023-03-15 15:12:08 +0300

    TestIteration2 pass

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import (
+	//"fmt"
+	"log"
+
+	"net/http"
+
+	"github.com/aaarkadev/collectalertagent/internal/handlers"
+	. "github.com/aaarkadev/collectalertagent/internal/repositories"
+	. "github.com/aaarkadev/collectalertagent/internal/storages"
+)
+
+func initRepo(r Repo) {
+	r.Init()
+	//fmt.Println("repo", r)
+	/*
+		x, _ := r.Get("BuckHashSys")
+		fmt.Println("BuckHashSys", x)
+		log.Print(".")*/
+}
+
+func main() {
+	r := MemStorage{}
+	initRepo(&r)
+
+	mux := http.NewServeMux()
+	h := handlers.UpdateMetricsHandlerStruct{RepoData: &r}
+	mux.HandleFunc("/", h.Handler)
+
+	//log.Println("server start http://127.0.0.1:8080")
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/aaarkadev/collectalertagent
 
 go 1.18
+
+replace github.com/aaarkadev/collectalertagent/internal/handlers => ./internal/handlers
+replace github.com/aaarkadev/collectalertagent/internal/repositories => ./internal/repositories
+replace github.com/aaarkadev/collectalertagent/internal/storages => ./internal/storages
+replace github.com/aaarkadev/collectalertagent/internal/types => ./internal/types
